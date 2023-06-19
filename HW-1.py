cea6af48dab6b18aa34f5cbb96462ca6a9a97810
Alexandr Smirnov <alexsmirdocs@gmail.com>
2023-06-19 21:18:40 +0300

    change variables and magic methods

--- HW-1.py
+++ HW-1.py
@@ -24,12 +24,24 @@ components = ['Mouse', 'Keyboard']
 print(components)
 
 # Расширить 1ый список элементами 2го списка
-print(pc_details.__add__(components))
+pc_details.extend(components)
+
+print(pc_details)
 
 # Создать два списка
-countries = ['Russia', 'China', 'Germany', 'France']
-cities = ['Moscow', 'Pekin', 'Berlin', 'Paris']
+first_list = ['Russia', 'China', 'Germany', 'France']
+second_list = [True, False, {'b': 10}, 2.5]
 
-world = countries + cities
+merged_list = first_list + second_list
 
-print(world)
+print(merged_list)
+
+# Мерж листов с магическим методом
+other_merged_list = first_list.__add__(second_list)
+
+print(other_merged_list)
+
+# Другой порядок
+other_merged_list = second_list.__add__(first_list)
+
+print(other_merged_list)
