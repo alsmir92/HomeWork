e73aac6b5779a96e910a3340a12e5d1368161c7e
Alexandr Smirnov <alexsmirdocs@gmail.com>
2023-06-21 22:14:13 +0300

    new task 2(dict)

---
+++ HW_2.py
@@ -0,0 +1,22 @@
+# Создайте пустой словарь
+my_dict = {}
+# Трижды попросите пользователя сначала ввести название ключа, а потом ввести значение для этого ключа. Всего должно быть 6 запросов на ввод текста.
+key_1 = input("Please enter key 1: ")
+value_1 = input("Please enter value 1: ")
+
+my_dict[key_1] = value_1
+
+key_2 = input("Please enter key 2: ")
+value_2 = input("Please enter value 2: ")
+
+my_dict[key_2] = value_2
+
+key_3 = input("Please enter key 3: ")
+value_3 = input("Please enter value 3: ")
+
+my_dict[key_3] = value_3
+
+print(my_dict)
+# Во время получения данных от пользователя добавляйте в словарь ключи с значениями согласно тому, что ввел пользователь.
+
+# Выведите редзультирующий словарь в терминал
