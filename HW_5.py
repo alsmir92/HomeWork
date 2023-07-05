310538e44063e0243b0c5820ffe7e6d70f9f8762
Alexandr Smirnov <alexsmirdocs@gmail.com>
2023-07-05 17:07:02 +0300

    key words args

--- HW_5.py
+++ HW_5.py
@@ -14,3 +14,28 @@ print(weapon_two)
 
 weapon_three = merge_lists_to_dict([10, False, 'cry'], ['Bomb'])
 print(weapon_three)
+
+
+# Функция с key words args. Вызов функции с позиционным аргументом и ключевым
+def merge_lists_to_dict(ammo, countries):
+    return dict(zip(ammo, countries))
+
+
+weapon = merge_lists_to_dict(
+    ammo=['AK-47', 'M16', 'SCAR-L'], countries=['USSR', 'USA', 'Belgium'])
+print(weapon)
+
+weapon_two = merge_lists_to_dict([10, False, 'cry'], countries=['Bomb'])
+print(weapon_two)
+
+
+# Создать функцию, в которой все именованные аргументы будут объединяться в словарь.
+def update_car_info(**car):
+    car['is_available'] = True
+    return car
+
+
+print(update_car_info(brand='BMW', price=100000))
+
+# TypeError: update_car_info() takes 0 positional arguments but 2 were given
+print(update_car_info('BMW', 100000))
