bcdd3f33248c3c1c7368624ea611d74e40f4abb1
Alexandr Smirnov <alexsmirdocs@gmail.com>
2023-07-26 00:16:02 +0300

    logical operators

---
+++ HW_8.py
@@ -0,0 +1,7 @@
+my_dict = {'BMW': 'M5', 'Mercedes': '200'}
+other_dict = {'Mercedes': '200', 'BMW': 'M5'}
+
+if my_dict == other_dict:
+    print("Dictionaries are equel")
+if my_dict != other_dict:
+    print("Dictionaries are different")
