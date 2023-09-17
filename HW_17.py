14531c9b2be7e7deba1ab3d7d0aa2c10bc2612f2
Alexandr Smirnov <alexsmirdocs@gmail.com>
2023-09-17 14:45:20 +0300

    add info about authenticated user (decorator)

--- HW_17.py
+++ HW_17.py
@@ -25,3 +25,44 @@ def sum(a, b):
 
 
 print(sum(a=40.3, b=20))
+
+# Function name: mult
+# Function arguments: (5, 2), {}
+# Function result: 10
+# 10
+
+# Проверка аутентификации пользователя
+
+
+def is_user_authenticated():
+    return True  # /False
+
+
+def check_user_authenticated(fn):
+    def wrapper(*args, **kwargs):
+        if is_user_authenticated():
+            print("User is authenticated!")
+            return fn(*args, **kwargs)
+        else:
+            raise Exception("User is NOT authenticated")
+
+    return wrapper
+
+
+@check_user_authenticated
+def do_sensitive_job():
+    # Do sime tasks only if user is authenticated
+    print("Results of some sensitive tasks")
+
+
+try:
+    do_sensitive_job()
+except Exception as e:
+    print(e)
+
+# True
+# User is authenticated!
+# Results of some sensitive tasks
+
+# False
+# User is NOT authenticated
