dcf70d3cef50af3dcc28f2a390c3befc738c3f6f
Alexandr Smirnov <alexsmirdocs@gmail.com>
2023-10-04 18:57:10 +0300

    отправка вложений: html и jpg

--- EmailMessage.py
+++ EmailMessage.py
@@ -1,12 +1,23 @@
 from email.message import EmailMessage
 import smtplib
+from string import Template
+from pathlib import Path
+
 
 my_email = EmailMessage()
 
-my_email['from'] = 'Alex'
-my_email['to'] = 'test@gmail.com'
-my_email['subject'] = "Hello from Python"
-my_email.set_content()
+html_template = Template(Path("templates/index.html").read_text())
+html_content = html_template.substitute({'name': 'Alex', 'date': 'tomorrow'})
+
+my_email['from'] = 'Alex <bs@gmail.com'
+my_email['to'] = 'friend@gmail.com'
+my_email['subject'] = "Email with image"
+my_email.set_content(html_content, 'html')
+
+with open('images/пикча.jpg', 'rb') as img:
+    image_data = img.read()
+    my_email. add_attachment(image_data, maintype='image',
+                             subtype='jpg', filename='пикча.jpg')
 
 with smtplib.SMTP(host='localhost', port=2525) as smtp_server:
     smtp_server.ehlo()
